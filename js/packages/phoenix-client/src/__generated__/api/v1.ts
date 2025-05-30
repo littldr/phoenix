78eeae21fd7bfce046a80f2476a9ec2544010198
Dustin Ngo <dustin@arize.com>
2025-05-30 15:55:24 -0400

    Rebuild openapi schema

--- js/packages/phoenix-client/src/__generated__/api/v1.ts
+++ js/packages/phoenix-client/src/__generated__/api/v1.ts
@@ -2046,10 +2046,7 @@ export interface components {
             /** Next Cursor */
             next_cursor: string | null;
         };
-        /**
-         * SpanContext
-         * @description Context propagation for a span
-         */
+        /** SpanContext */
         SpanContext: {
             /**
              * Trace Id
@@ -2062,12 +2059,7 @@ export interface components {
              */
             span_id: string;
         };
-        /**
-         * SpanEvent
-         * @description A Span Event can be thought of as a structured log message (or annotation)
-         *     on a Span, typically used to denote a meaningful, singular point in time
-         *     during the Span's duration.
-         */
+        /** SpanEvent */
         SpanEvent: {
             /**
              * Name
@@ -3361,7 +3353,7 @@ export interface operations {
     spanSearch: {
         parameters: {
             query?: {
-                /** @description Pagination cursor (GlobalID of Span) */
+                /** @description Pagination cursor (Phoenix Span ID) */
                 cursor?: string | null;
                 /** @description Maximum number of spans to return */
                 limit?: number;
@@ -3424,7 +3416,7 @@ export interface operations {
     spanSearchPhoenix: {
         parameters: {
             query?: {
-                /** @description Pagination cursor (GlobalID of Span) */
+                /** @description Pagination cursor (Phoenix Span ID) */
                 cursor?: string | null;
                 /** @description Maximum number of spans to return */
                 limit?: number;
